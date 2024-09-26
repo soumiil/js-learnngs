0a017a06be26dc0af65988c45459133e9860912d
Soumil Agarwal <agarwalsoumil6@gmail.com>
2024-09-26 13:28:14 +0530

    new

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,21 @@
+// const balance = 1000;
+
+// // if(balance > 500) console.log("test");
+
+// if(balance < 500) {
+//     console.log("Less than 500");
+// } else if (balance < 900) {
+//     console.log("Less than 900");
+// } else {
+//     console.log("Less than 1200");
+// }
+
+const userLoggedIn = true, debitCard = true;
+
+if (userLoggedIn && debitCard) {
+    console.log("Allowed to buy any course");
+}
+
+if (userLoggedIn) {
+    
+}
